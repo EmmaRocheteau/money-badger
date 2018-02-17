2d9c689df6e61110a81bd6da894a2b706cb10b07
Abhishek Ambekar <a.ambekar@cantab.net>
2018-02-17 22:31:27 +0000

    Data aggregation from sources working
    Creates a pandas dataframe

--- matchup.py
+++ matchup.py
@@ -1,13 +1,18 @@
 import parsing_splitwise as sp
 import parsing_starling as st
+import numpy as np
+#from flask import session
+import pandas as pd
+from itertools import combinations
+from datetime import timedelta as td
 
 # Get data from starling and format to a pandas dataframe.
 stdata = st.json_read('data/sample-starling-data.txt')
 cleaned = st.cleanup(stdata)
 
-
 # Get data from spltwise and format to a pandas dataframe.
 spdata = sp.yaml_read('.\data\expenses.yaml')
+#spdata = session['expenses']
 mine = sp.get_owed(spdata, user_id=5090950)
 df = sp.cleanup(mine, user_id=5090950)
 
@@ -15,6 +20,70 @@ df = sp.cleanup(mine, user_id=5090950)
 df = df[df['Currency'] == 'GBP']
 df2 = sp.date_filter(df, date_from='15/07/2017', to='15/07/2017')
 
-print(df2[['Category', 'Currency', 'Date', 'Description', 'Paid', 'Payment']])
+df3 = df2[['Category', 'Currency', 'Date', 'Description', 'Paid', 'Payment']]
+df3 = df3.rename(index=str, columns={"Paid": "Cost"})
 
 
+def compare_dfs(st_df, sp_df):
+    # create a list for what has actually been spent.
+    print(st_df, sp_df)
+    big_list = []
+    matched_pairs = []
+    for i in range(len(st_df['Cost'])):
+        match_found = False
+        unique_inds = np.where(sp_df['Cost'] == st_df['Cost'][i])[0]
+        for ind in unique_inds:
+            if sp_df.iloc[ind - 5, 2] <= st_df.iloc[i, 2] <= sp_df.iloc[ind + 5, 2]:
+                # there is a match between the entries in the two data frames.
+                big_list.append([*st_df.iloc[i, :], 'both'])
+                matched_pairs.append([i, ind])
+                match_found = True
+        if not match_found:
+            # this is a personal expense, logged only on starling
+            big_list.append([*st_df.iloc[i, :], 'starling'])
+    print(matched_pairs)
+    for j in range(len(sp_df['Cost'])):
+        print(type(j))
+        if j not in np.array(matched_pairs)[:, 1]:
+            big_list.append([*sp_df.iloc[j, :], 'splitwise'])
+    return big_list
+
+
+def compare_and_merge(df1, df2):
+    df1['Category'] = np.nan
+    df1['Group ID'] = np.nan
+    df1['Payment'] = np.nan
+    df1['Owe'] = np.nan
+    df1['Source'] = 'starling'
+    df2['Source'] = 'splitwise'
+    combined = pd.concat([df1, df2])
+    sorted = combined.sort_values('Date')
+    unique_costs = np.unique(pd.to_numeric(sorted['Cost']))
+    rows_to_delete = []
+    rows_to_both = []
+    for cost in unique_costs:
+        repeats = np.where(sorted['Cost'] == cost)[0]
+        if len(repeats) >= 2:
+            for pair in list(combinations(repeats, 2)):
+                if sorted.iloc[pair[0], 2] - td(days=5) <= sorted.iloc[pair[1], 2] <= sorted.iloc[pair[0], 2] + td(days=5):
+                    # repeated transaction
+                    if sorted.iloc[pair[0], -1] == 'starling':
+                        #sorted = sorted.drop(sorted.index[pair[0]])
+                        #sorted.iloc[pair[1], -1] = 'both'
+                        rows_to_delete.append(pair[0])
+                        rows_to_both.append(pair[1])
+                    else:
+                        #sorted = sorted.drop(sorted.index[pair[1]])
+                        #sorted.iloc[pair[0], -1] = 'both'
+                        rows_to_delete.append(pair[1])
+                        rows_to_both.append(pair[0])
+    sorted.iloc[rows_to_both, -1] = 'both'
+    rows_to_delete.sort(reverse=True)
+    sorted = np.delete(np.array(sorted), rows_to_delete, 0)
+    return pd.DataFrame(sorted, columns=['Cost', 'Currency', 'Date',
+                                         'Description', 'Category',
+                                         'Group ID', 'Payment', 'Owe',
+                                         'Source'])
+
+
+print(compare_and_merge(cleaned, df3))#df3)
