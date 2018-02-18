b387bcd9c63f7b16bb3777cb17049f71a63cdfbf
Gareth Funk <gareth.funk94@gmail.com>
2018-02-18 11:18:39 +0000

    Changed home page. tweaks to final run

--- machine_learning/make_dataframes.py
+++ machine_learning/make_dataframes.py
@@ -6,7 +6,7 @@ import pandas as pd
 def dataframer():
     graph_data = pd.read_csv('../graph_data.csv')
     prediction_data = pd.read_csv('../prediction_data.csv')
-    #actual_data = pd.read_csv('../actual_data_balance.csv')
+
     df_for_plotting = pd.merge(graph_data, prediction_data, on='Date', how='outer')
     #df_for_plotting = pd.merge(df_for_plotting, actual_data, on='Date',
     # how='outer')
